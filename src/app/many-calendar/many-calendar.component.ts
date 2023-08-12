1b43e9944268141d5234816df8304726e8a58851
nawajar <nawapb@gmail.com>
2023-08-12 17:34:22 +0700

    extra

--- src/app/many-calendar/many-calendar.component.ts
+++ src/app/many-calendar/many-calendar.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-many-calendar',
   templateUrl: './many-calendar.component.html',
-  styleUrls: ['./many-calendar.component.scss']
+  styleUrls: ['./many-calendar.component.scss'],
 })
 export class ManyCalendarComponent {
+  month = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
 
+  transformMonth(monthIdx: number): string {
+    return this.month[monthIdx];
+  }
 }
